1c366d5db4c9c88acafe2e62d48d1ed1cbbf394a
Ezra Brewer <ezjbrewer@gmail.com>
2024-06-10 13:20:36 -0500

    Removed test variable from previous branch on Home.jsx

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -6,8 +6,6 @@ export const Home = () => {
     
     const welcomeMsgPart1 = "After an unsuccessful dog walking business, brothers Camillo and Giuseppe decided to throw their hand into crafting artisan sandwiches from their Sicilian hearts.";
     const welcomeMsgPart2 = "Fast-forward forty years to today, and they are now bringing their sandwiches to customers digitally. Try from the many different variations upon which a sandwich could be made!";
-    
-    const seeThisVariable = "This variable is to establish whether or not I can make a pull request within a created branch and merge that pull request."
 
     return(
         <div>
