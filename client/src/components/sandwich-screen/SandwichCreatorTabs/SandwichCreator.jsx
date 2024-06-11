89e9ad9a3dece1390c5b7b7aebf8937f789a75b5
Ezra Brewer <ezjbrewer@gmail.com>
2024-06-11 09:56:47 -0500

    Created get ingredients endpoints that is dependent on an input which will dictate what ingredients are returned.

--- client/src/components/sandwich-screen/SandwichCreatorTabs/SandwichCreator.jsx
+++ client/src/components/sandwich-screen/SandwichCreatorTabs/SandwichCreator.jsx
@@ -1,5 +1,32 @@
-export const SandwichCreator = () => {
+import { Button } from "reactstrap"
+
+export const SandwichCreator = ({setCurrentSandwich}) => {
     return(
-        <div>Test!</div>
+        <div className="sandwich-create-view">
+            <div className="tab-container">
+                <div className="tab-options">
+                    <Button>
+                        Bread
+                    </Button>
+                    <Button>
+                        Meats
+                    </Button>
+                    <Button>
+                        Toppings
+                    </Button>
+                </div>
+                <div className="complete-sandwich-btn-container">
+                    <Button>
+                        Complete Sandwich
+                    </Button>
+                </div>
+            </div>
+            <div className="ingredients-container">
+
+            </div>
+            <div className="sandwich-display-container">
+
+            </div>
+        </div>
     )
 }
